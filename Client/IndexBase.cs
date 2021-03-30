2c0747df481d4b345b48d11e6835325406a2e695
boredanyway <ben.strohmann@gmail.com>
2021-03-30 17:46:12 +0200

    index base cs

--- Client/IndexBase.cs
+++ Client/IndexBase.cs
@@ -116,10 +116,10 @@ namespace Oqtane.ChatHubs
                 string hostname = new Uri(NavigationManager.BaseUri).Host;
                 this.ChatHubService.IdentityCookie = new Cookie(".AspNetCore.Identity.Application", await this.CookieService.GetCookieAsync(".AspNetCore.Identity.Application"), "/", hostname);
 
+                await this.JSRuntime.InvokeVoidAsync("showchathubscontainer");
+
                 await this.BrowserResizeService.RegisterWindowResizeCallback();
                 await BrowserHasResized();
-
-                await this.JSRuntime.InvokeVoidAsync("showchathubscontainer");
             }
 
             await base.OnAfterRenderAsync(firstRender);

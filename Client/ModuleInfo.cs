057f0b65fd9c1e8b9316d96b459ebb31dd45a080
Benjamin Strohmann <ben.strohmann@gmail.com>
2021-07-27 12:03:25 +0200

    package 4.1.7

--- Client/ModuleInfo.cs
+++ Client/ModuleInfo.cs
@@ -9,9 +9,9 @@ namespace Oqtane.ChatHubs
         {
             Name = "ChatHub",
             Description = "ChatHub",
-            Version = "4.1.6",
+            Version = "4.1.7",
             ServerManagerType = "Oqtane.ChatHubs.Manager.ChatHubManager, Oqtane.ChatHubs.Server.Oqtane",
-            ReleaseVersions = "1.0.0,4.1.6",
+            ReleaseVersions = "1.0.0,4.1.7",
             Dependencies = "Oqtane.ChatHubs.Shared.Oqtane",
             SettingsType = "Oqtane.ChatHubs.Settings, Oqtane.ChatHubs.Client.Oqtane"
         };
